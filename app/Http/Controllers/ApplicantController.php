fa6059d6bfc54fe09f8724f51d673d3da28f10b3
yogesNedin <yoges.nedinsellian@pipeline.com.my>
2021-05-20 14:22:45 +0800

    Update ApplicantController.php

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -44,7 +44,7 @@ class ApplicantController extends Controller
 
     public function retrieveStatus(Request $request){
         $user = auth('api')->user();
-        $data = DB::table('permohonan')->where('id_pengguna',$user['id'])->get();
+        $data = DB::table('Permohonan')->where('id_pengguna',$user['id'])->get();
         $data->type = $data->jenis_permohonan == "p1" ? "Permohonan Baharu" : "";
         return response()->json($data,200);
     }
